1492ac426e7745e8aaf61d9028a3603932b98807
Nguyen Thanh Duy Tan <33972583+kirichain@users.noreply.github.com>
2024-03-23 10:43:26 +0700

    remove unnecessary lines

--- Source/RFID/src/main.cpp
+++ Source/RFID/src/main.cpp
@@ -13,40 +13,11 @@ void setup() {
     Serial.begin(115200);
     Serial2.begin(115200);
     mediator.init_services();
-//    mediator.taskArgs.feature = BOOT;
-//    mediator.set_current_feature();
-//    mediator.execute_task(RENDER_FEATURE);
-//    mediator.execute_task(LOAD_FS);
-//    if ((mediator.taskResults.isFsLoaded)) {
-//        mediator.taskArgs.operatingMode = mediator.taskResults.savedOperatingModeInFs;
-//        if (mediator.taskResults.savedOperatingModeInFs == HANDHELD) {
-//            mediator.taskArgs.feature = HOME_HANDHELD_1;
-//        } else {
-//            mediator.taskArgs.feature = HOME_TERMINAL;
-//        }
-//    } else {
-//        mediator.taskArgs.operatingMode = HANDHELD;
-//    }
-//    mediator.set_current_feature();
-//    mediator.execute_task(RENDER_FEATURE);
 
-//    mediator.taskArgs.operatingMode = HANDHELD;
-//    mediator.execute_task(SET_OPERATING_MODE);
     mediator.execute_task(INIT_STA_WIFI);
     mediator.taskArgs.feature = HOME_HANDHELD_2;
     mediator.execute_task(RENDER_FEATURE);
     mediator.set_current_feature();
-    //mediator.execute_task(INIT_AP_WIFI);
-
-//For testing, we execute task BLINK_LED and stop this task when we receive message from MQTT broker
-//    mediator.taskArgs.task = BLINK_SCREEN;
-//    mediator.set_current_task();
-//    mediator.set_current_task_status(false);
-//    mediator.execute_task(mediator.taskArgs.task);
-//    mediator.taskArgs.task = INIT_STA_WIFI;
-//    mediator.set_current_task();
-//    mediator.set_current_task_status(false);
-//    mediator.execute_task(mediator.taskArgs.task);
 }
 
 void loop() {
@@ -57,9 +28,6 @@ void loop() {
     mediator.execute_task(READ_SERIAL_COMMUNICATION_MESSAGE);
     mediator.execute_task(HANDLE_MQTT_MESSAGE);
     mediator.execute_task(CHECK_WIFI_CONNECTION);
-
-//    mediator.taskArgs.task = mediator.taskResults.currentTask;
-//    mediator.set_current_task();
 
     if ((mediator.isTaskExecutable) && (mediator.taskArgs.task != NO_TASK) && (mediator.taskArgs.task != IDLE)) {
         Serial.println(F("Task execution starts"));
@@ -74,9 +42,7 @@ void loop() {
         }
 
         Serial.println(F("Task execution completed"));
-        //mediator.set_current_task_status(true);
         mediator.taskArgs.task = IDLE;
         mediator.set_current_task();
-        //mediator.set_current_task_status(false);
     }
 }
